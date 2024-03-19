2cf240e3f7a088c7ed2451955199ad02def13ad1
AnthonyAlm <anthony.almany@gmail.com>
2024-03-19 15:28:12 +1100

    add header and navbar

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,35 @@
-import Counter from '../components/Counter';
-import About from "./About";
+// import Counter from '../components/Counter';
+// import About from "./About";
+import NavBar from "../components/NavBar/NavBar";
+import header from "../assets/images/header.jpg"
+
+
 const View = () => {
+
+
+
     return (
-        <div className='home'>
-            <p>Home</p>
-            <Counter type="edit" />
-            <About />
+        <div className='home bg-white h-2/3'>
+
+            <header className="bg-cover relative" style={{ backgroundImage: `url(${header})`, height: "100%", opacity: "80%" }}>
+                <NavBar />
+                <div className="flex flex-col gap-4 absolute bottom-12 left-28">
+                    <h2 className="flex flex-col text-6xl text-white font-manrope leading-82">
+                        <span>Graduate Careers</span>
+                        <span className="mt-4">Expo 2024</span></h2>
+                    <h3 className=" text-xl text-white font-manrope font-light leading-36">The Graduate Careers Expo is back on Campus in 2024!</h3>
+                    <h3 className="text-xl text-white font-manrope font-light leading-36 underline">LEARN MORE</h3>
+                </div>
+            </header>
+
+            <div className="h-1/3 flex items-center gap-8 absolute left-28">
+                <div className="posts-title text-red_primary font-marcellus text-7xl font-normal leading-100">Forum hot posts</div>
+                <div className="posts-title text-red_primary font-marcellus text-3xl font-normal leading-100 border-b border-solid border-red_primary"> &gt;All posts</div>
+
+            </div>
+
+            {/* <Counter type="edit" /> */}
+            {/* <About /> */}
         </div>
     );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,21 @@ module.exports = {
       'md': { 'max': '768px' },
       'sm': { 'max': '640px' },
     },
-    extend: {},
+    extend: {
+      colors: {
+        primary_text: "rgba(255, 56, 92, 1)",
+        secondary_text: "rgba(57, 57, 57, 1)",
+        white_text: "rgba(255, 255, 255, 1)",
+        red_primary: "rgba(255, 56, 92, 1)",
+        red_secondary: "rgba(153, 34, 55, 1)",
+      },
+      fontFamily: {
+        roboto: ['Roboto', 'sans-serif'],
+        manrope: ['Manrope', 'sans-serif'],
+        inter: ['Inter', 'sans-serif'],
+        marcellus: ["marcellus, sans-serif"]
+      },
+    },
   },
   plugins: [],
 }
